d3cf4516affcd594e477d4ef0d8bbd51577fcdde
nurdeka <75995236+nurdeka@users.noreply.github.com>
2020-12-14 21:52:39 +0700

    Add files via upload

---
+++ pythonday1.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+kardus = ['aqua', 'sprite', 'cocacola', 'fanta']
+print(kardus)
+
+
+# In[11]:
+
+
+botol = 'sprite'
+print(botol)
+
+
+# In[12]:
+
+
+print(kardus[3])
+
+
+# In[14]:
+
+
+print(kardus[-1])
+
+
+# In[15]:
+
+
+kardus[0]='cleo'
+print(kardus)
+
+
+# In[17]:
+
+
+kardus[-1]=80
+print(kardus)
+
+
+# In[18]:
+
+
+print(kardus[1:3])
+
+
+# In[19]:
+
+
+print(kardus[1:4])
+
+
+# In[21]:
+
+
+kardus[1:3]=['chitato', 'chiki', 'cheetos', 'jetzet', 'qtela', 'oreo', 'malkist', 'doritos', 'taro']
+print(kardus)
+
+
+# In[23]:
+
+
+kasir=('aqua', 'sprite', 'cocacola', 'fanta')
+kasir[0]='chitato'
+
+
+# In[24]:
+
+
+kardus_label={'aqua':'nurdeka', 'sprite':'arif', 'cocacola':'thomas', 'fanta':'tio'}
+print(kardus_label)
+
+
+# In[25]:
+
+
+print(kardus_label['aqua'])
+
+
+# In[26]:
+
+
+print(kardus_label['fanta'])
+
+
+# In[27]:
+
+
+kardus_label['dancow']='khafid'
+print(kardus_label)
+
+
+# In[28]:
+
+
+kardus_label['baterai']=['ajie','husein']
+print(kardus_label)
+
+
+# In[29]:
+
+
+kardus_label['mars']={'lampu': 'mars', 'gunting':'mars'}
+print(kardus_label)
+
+
+# In[ ]:
+
+
+
+
